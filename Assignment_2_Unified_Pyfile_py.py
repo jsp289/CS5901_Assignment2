7e52baf54fa95fc9a988d17c3e3269949e177998
jsp289 <38367490+jsp289@users.noreply.github.com>
2025-05-20 17:53:18 -0400

    Update Assignment_2_Unified_Pyfile_py.py
    Line 8 -> Changed "true" to boolean True

--- Assignment_2_Unified_Pyfile_py.py
+++ Assignment_2_Unified_Pyfile_py.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyN2w/cOtr0E37p7cmcqjYaa",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
